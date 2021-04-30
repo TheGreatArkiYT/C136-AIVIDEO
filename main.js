a887827c100c06ce9531586633be7ebcb88751aa
Arka Bansal <github.arkbansal@gmail.com>
2021-04-30 13:00:09 +0530

    1
    2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects = "";
+status = "";
 video = "";
 function preload() {
     video = createVideo('video.mp4');
@@ -9,6 +11,20 @@ function setup() {
 }
 function draw() {
     image(video , 0, 0, 480 , 280);
+    if (status != "") {
+        objectDetector.detect(video , gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number Of Objects : " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[1].confidence * 100);
+            text(objects[i].label + " " + percent + "% " , objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 function start() {
     objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
@@ -20,4 +36,11 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error , results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
